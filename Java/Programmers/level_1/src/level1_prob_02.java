685dadc5f7792e0bdd55a0a48b5d62a438bacf77
종찬 <ieejo716@naver.com>
2021-01-24 21:03:32 +0900

    프로그래머스 문제 풀이 level1 내적

---
+++ Java/Programmers/level_1/src/level1_prob_02.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class level1_prob_02 {
+    static int solution(int[] a, int[] b){
+        int answer = 0;
+        int[] sum = new int[a.length];
+
+        for(int i=0;i<sum.length;i++){
+            sum[i] = a[i] * b[i];
+        }
+
+        for(int i=0;i<sum.length;i++){
+            answer += sum[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4};
+        int[] b = {-3,-1,0,2};
+        int answer = solution(a,b);
+
+        System.out.println(answer);
+    }
+}
