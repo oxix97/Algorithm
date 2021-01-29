4ec9833390075b5111dcb8b35544e651a6190f4d
종찬 <ieejo716@naver.com>
2021-01-29 21:14:39 +0900

    프로그래머스_레벨1_체육복

---
+++ Java/Programmers/level_1/src/level1_prob_05.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class level1_prob_05 {
+    private static String id;
+    public static String NullCount(){
+        String ans = "";
+        for(int i=0;i<id.length();i++) ans += "a";
+        return ans;
+    }
+    public static String solution(String new_id){
+        String answer = new_id.toLowerCase(); //1단계
+
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input : ");
+        id = scanner.next();
+
+    }
+}
