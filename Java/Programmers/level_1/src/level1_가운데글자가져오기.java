3b1c807561a8b50d479baed5c6d311cb3434c0ae
종찬 <ieejo716@naver.com>
2021-01-30 22:51:31 +0900

    가운데 글자 가져오기

---
+++ Java/Programmers/level_1/src/level1_가운데글자가져오기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class level1_가운데글자가져오기 {
+    public static String solution(String s) {
+        String answer = "";
+        int index = s.length();
+        if(index %2 == 1)
+            answer = s.substring(index/2, index/2+1);
+        else
+            answer = s.substring(index/2-1, index/2+1);
+        return answer;
+    }
+    public static void main(String[] args) {
+        String s = "abcde";
+        String r = "qwer";
+
+        String ss = solution(s);
+        String rr = solution(r);
+
+        System.out.println(ss);
+        System.out.println(rr);
+    }
+}
