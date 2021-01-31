52c0625c4e14c7a5855eeda0156b81474f481dce
종찬 <ieejo716@naver.com>
2021-01-31 21:15:45 +0900

    문자열 내 p와 y의 개수

---
+++ Java/Programmers/level_1/src/level1_문자열내p와y의개수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class level1_문자열내p와y의개수 {
+    public static boolean solution (String s) {
+        int countP = 0;
+        int countY = 0;
+        String str = s.toLowerCase();
+
+        for(int i=0; i<str.length();i++) {
+            if(s.charAt(i) == 'p') countP++;
+            if(s.charAt(i)== 'y') countY++;
+        }
+        if(countP==countY)
+            return true;
+        return false;
+    }
+    public static void main(String[] args) {
+        String s1 = "pPoooyY";
+        String s2 = "Pyy";
+
+        boolean ans1 = solution(s1);
+        boolean ans2 = solution(s2);
+
+        System.out.println("s1 is true? : "+ans1);
+        System.out.println("s2 is true? : "+ans2);
+    }
+}
