c1661a857a96cecc59a44981fc44552a806564ad
Chan <oxix97@git.com>
2021-02-05 20:45:03 +0900

    백준 1271번

---
+++ Java/Baekjoon/src/Number_1271.java
@@ -0,0 +1,15 @@
+import sun.java2d.pipe.BufferedRenderPipe;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+public class Number_1271 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        BigInteger n = new BigInteger(tokenizer.nextToken());
+        BigInteger m = new BigInteger(tokenizer.nextToken());
+
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+    }
+}
