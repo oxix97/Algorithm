baae31f7f005df439179d0956d1e77e27acd5e96
Chan <ieejo716@naver.com>
2021-02-07 12:00:11 +0900

    신규 아이디 추천 53.8

--- Java/Programmers/level_1/src/level1_신규아이디추천.java
+++ Java/Programmers/level_1/src/level1_신규아이디추천.java
@@ -19,18 +19,51 @@ public class level1_신규아이디추천 {
                 if(str.equals("-") || str.equals("_") || str.equals(".")) answer += str;
             }
         }
+        try{
+            while(answer.charAt(0) == '.') answer = answer.substring(1,answer.length()-1); //4단계
+            while(answer.charAt(answer.length()-1) == '.') answer = answer.substring(0,answer.length()-2);
+        }catch(StringIndexOutOfBoundsException e){
+            answer  = "a".repeat(new_id.length()); //5단
+        }
 
+        String str = ""; //3단계
+        for(int i=0;i<answer.length();i++){
+            if(answer.charAt(i) == '.' && answer.charAt(i) == answer.charAt(i+1))
+                continue;
+            str += answer.charAt(i);
+        }
+        answer = str;
 
+        if(answer.length()>=16) answer = answer.substring(0,15);
+
+        while(answer.charAt(answer.length()-1) == '.') answer = answer.substring(0,answer.length()-1); //6단계
+
+        while(answer.length()<=2) answer += answer.charAt(answer.length()-1); //7단계
 
         return answer;
     }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("input : ");
-        String input = scanner.nextLine();
-        String answer = solution(input);
+        String str1 = "...!@BaT#*..y.abcdefghijklm";
+        String str2 = "z-+.^.";
+        String str3 = "=.=";
+        String str4 = "123_.def";
+        String str5 = "abcdefghijklmn.p";
+        String cus = "...";
 
-        System.out.println(answer);
+        String result1 = solution(str1);
+        String result2 = solution(str2);
+        String result3 = solution(str3);
+        String result4 = solution(str4);
+        String result5 = solution(str5);
+        String recus = solution(cus);
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(recus);
+
 
     }
 }
