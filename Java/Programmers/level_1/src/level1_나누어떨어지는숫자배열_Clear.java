804b9fd2000e0e84902185fe76085bee83a470f7
Chan <ieejo716@naver.com>
2021-02-09 00:14:51 +0900

    나누어 떨어지는 숫자 배열

--- Java/Programmers/level_1/src/level1_나누어떨어지는숫자배열_Clear.java
+++ Java/Programmers/level_1/src/level1_나누어떨어지는숫자배열_Clear.java
@@ -2,9 +2,10 @@ import java.util.Arrays;
 
 public class level1_나누어떨어지는숫자배열_Clear {
     public static int[] solution(int[] arr, int divisor){
-        int[] answer = Arrays.stream(arr).filter(factor -> factor % divisor == 0).toArray();
+        int[] answer = Arrays.stream(arr).
+                filter(factor -> factor % divisor == 0).toArray();
         if(answer.length == 0) answer = new int[] {-1};
-        Arrays.sort(answer);
+        else Arrays.sort(answer);
         return answer;
     }
     public static void main(String[] args) {
