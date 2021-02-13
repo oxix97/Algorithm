0d2e60add771bb7d5aa3727f102b575483040eec
Chan <ieejo716@naver.com>
2021-02-14 00:13:19 +0900

    문자열 정수로 바꾸기

--- Java/Programmers/level_1/src/level1_문자열을정수로바꾸기.java
+++ Java/Programmers/level_1/src/level1_문자열을정수로바꾸기.java
@@ -4,18 +4,19 @@ public class level1_문자열을정수로바꾸기 {
         return Integer.parseInt(s);
     }
     public static int solution2(String s){
-        boolean sign = true;
-        int num = 0;
+        boolean count = true;
+        int result = 0;
+        int i = 0;
+        if(s.charAt(0) == '+') i=1;
+        else if(s.charAt(0) == '-') { i=1; count = false; }
 
-        for(int i=1;i<s.length();i++)
-            num = num * 10 + (s.charAt(i) - '0');
+        for(;i<s.length();i++)
+            result = result * 10 + (s.charAt(i) - '0');
 
-        if(s.charAt(0) == '-') sign = false;
-
-        return sign ? num : -1 *num;
+        return count ? result : -1 * result;
     }
     public static void main(String[] args) {
-        String str = "+1234";
+        String str = "-1234";
         int sol = solution2(str);
         System.out.println(sol);
 
