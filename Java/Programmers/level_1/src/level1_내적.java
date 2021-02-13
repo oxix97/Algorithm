ea5ed870a2ff5fa13007d60a4f71d990c921df15
Chan <ieejo716@naver.com>
2021-02-14 00:17:07 +0900

    내적

---
+++ Java/Programmers/level_1/src/level1_내적.java
@@ -0,0 +1,17 @@
+public class level1_내적 {
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for(int i=0;i<a.length;i++)
+            answer += a[i] * b[i];
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4};
+        int[] b = {-3,-1,0,2};
+
+        int answer = solution(a,b);
+        System.out.println(answer);
+    }
+}
