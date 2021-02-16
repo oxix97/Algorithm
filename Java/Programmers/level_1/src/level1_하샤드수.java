38cefbee8f95f47abf23dd00a721397c5d816944
Chan <ieejo716@naver.com>
2021-02-16 22:41:06 +0900

    하샤드 수

---
+++ Java/Programmers/level_1/src/level1_하샤드수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class level1_하샤드수 {
+    public static boolean solution(int x) {
+        String temp = Integer.toString(x);
+        int num = 0;
+        for(int i=0; i<temp.length();i++)
+            num += Integer.parseInt(temp.substring(i, i+1));
+
+        return x % num == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(11));
+        System.out.println(solution(12));
+    }
+}
