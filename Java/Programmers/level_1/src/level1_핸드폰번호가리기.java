24c88168eabb30298ebdf64949030b37f27b12c7
Chan <ieejo716@naver.com>
2021-02-18 08:16:32 +0900

    commit

--- Java/Programmers/level_1/src/level1_핸드폰번호가리기.java
+++ Java/Programmers/level_1/src/level1_핸드폰번호가리기.java
@@ -1,13 +1,14 @@
 import java.util.*;
 public class level1_핸드폰번호가리기 {
-    public static String solution(String phone_number) {
-        return "*".repeat(phone_number.length()-4)+phone_number.substring(phone_number.length()-4);
-    }
     /*public static String solution(String phone_number){
         //무슨 말이지??
         return phone_number.replaceAll(".(?=.{4})","*");
     }
     */
+    public static String solution(String phone_number) {
+        return "*".repeat(phone_number.length()-4)+phone_number.substring(phone_number.length()-4);
+    }
+
     public static void main(String[] args) {
         String s1 = "12222";
         String s2 = "023334444";
