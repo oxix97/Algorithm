f91a398b015aa02662ea372311ebf874f97abc61
Chan <ieejo716@naver.com>
2021-12-14 19:46:18 +0900

    Solution_1753

---
+++ Java/Baekjoon/최단경로/src/Solution_1753.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution_1753 {
+
+    public void dijkstra(int K) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int V = scan.nextInt();
+        int E = scan.nextInt();
+        int K = scan.nextInt();
+        int[][] arr = new int[E + 1][E + 1];
+        //정점 : V , 간선 : E , 시작점 : K
+
+        for (int i = 1; i < E + 1; i++) {
+            int u = scan.nextInt();
+            int v = scan.nextInt();
+            int w = scan.nextInt();
+
+        }
+    }
+}
