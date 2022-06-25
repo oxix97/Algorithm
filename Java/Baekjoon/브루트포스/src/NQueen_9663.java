ae21033a11fe632a7c1c31adfec5a5de5588dc89
username <ieejo716@naver.com>
2022-06-25 13:53:24 +0900

    git ignore

---
+++ Java/Baekjoon/브루트포스/src/NQueen_9663.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NQueen_9663 {
+    static int N;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][N];
+    }
+}
