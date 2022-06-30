e5d8c4e77ef1cbc65ad4c8f8fe1b0b71191278d2
username <ieejo716@naver.com>
2022-06-30 22:45:32 +0900

    백준-브루트포스-G5

---
+++ Java/Baekjoon/브루트포스/src/연구소_14502.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 연구소_14502 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
