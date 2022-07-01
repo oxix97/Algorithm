5c88f7b23e535856296b5ed9b66535ade7560e29
username <ieejo716@naver.com>
2022-07-01 17:48:34 +0900

    백준-브루트포스-G5_fail

--- Java/Baekjoon/브루트포스/src/연구소_14502.java
+++ Java/Baekjoon/브루트포스/src/연구소_14502.java
@@ -1,8 +1,28 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 연구소_14502 {
-    public static void main(String[] args) {
+    static int N;
+    static int M;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+
     }
 }
