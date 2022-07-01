ad67b8e571f3a494b51bb8ad924a298567ded7cf
username <ieejo716@naver.com>
2022-07-01 21:00:05 +0900

    백준-브루트포스-S1

--- Java/Baekjoon/브루트포스/src/연산자끼워넣기_14888.java
+++ Java/Baekjoon/브루트포스/src/연산자끼워넣기_14888.java
@@ -1,37 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class 연산자끼워넣기_14888 {
-    static int N;
-    static int[] num;
-    static int[] tools;
-
-    static final int ADD = 0;
-    static final int MINUS = 1;
-    static final int MULTIPLE = 2;
-    static final int DIVIDE = 3;
+    static int N, max, min;
+    static int[] num, tools, order;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-        num = new int[N];
-        tools = new int[4];
+        num = new int[N + 1];
+        tools = new int[5];
+        order = new int[N + 1];
+
+        max = Integer.MIN_VALUE;
+        min = Integer.MAX_VALUE;
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             num[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(num);
-
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++) {
+        for (int i = 1; i <= 4; i++) {
             tools[i] = Integer.parseInt(st.nextToken());
         }
 
+        recursion(1);
+        System.out.println(max);
+        System.out.println(min);
+
     }
+
+    static int calc() {
+        int value = num[1];
+
+        for (int i = 1; i <= N - 1; i++) {
+            if (order[i] == 1)
+                value = value + num[i + 1];
+            if (order[i] == 2)
+                value = value - num[i + 1];
+            if (order[i] == 3)
+                value = value * num[i + 1];
+            if (order[i] == 4)
+                value = value / num[i + 1];
+        }
+        return value;
+    }
+
+    private static void recursion(int k) {
+        if (k == N) {
+            int value = calc();
+            max = Math.max(max, value);
+            min = Math.min(min, value);
+        } else {
+            for (int i = 1; i <= 4; i++) {
+                if (tools[i] >= 1) {
+                    --tools[i];
+                    order[k] = i;
+                    recursion(k + 1);
+                    tools[i]++;
+                    order[k] = 0;
+                }
+            }
+        }
+    }
+
 }
