82cbfed07f830cbed22dbea37fd54c526937c5df
username <ieejo716@naver.com>
2022-07-02 12:52:55 +0900

    백준-DFS_S2

--- Java/Baekjoon/BFS_DFS/src/트리의부모찾기_11725.java
+++ Java/Baekjoon/BFS_DFS/src/트리의부모찾기_11725.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class 트리의부모찾기_11725 {
