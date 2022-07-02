de52cab8f8a59b44519d83cb0223d84728919ddb
username <ieejo716@naver.com>
2022-07-02 12:52:32 +0900

    백준-DFS_S2

---
+++ Java/Baekjoon/BFS_DFS/src/트리의부모찾기_11725.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class 트리의부모찾기_11725 {
+    static int N;
+    static ArrayList<Integer>[] list;
+    static boolean[] visited;
+    static int[] parents;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        list = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        parents = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        StringTokenizer st;
+        for (int i = 1; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list[x].add(y);
+            list[y].add(x);
+        }
+
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i])
+                dfs(i);
+        }
+
+        for (int i = 2; i <= N; i++) {
+            System.out.println(parents[i]);
+        }
+    }
+
+    private static void dfs(int v) {
+        if (visited[v]) return;
+        visited[v] = true;
+
+        for (int i : list[v]) {
+            if (!visited[i]) {
+                parents[i] = v;
+                dfs(i);
+            }
+        }
+    }
+}
