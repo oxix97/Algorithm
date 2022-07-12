6d49c6b1b137f071fec48f2ccf3f77d7048ec2fc
username <ieejo716@naver.com>
2022-07-13 00:20:27 +0900

    백준-이진탐색-S2-fail

---
+++ Java/Baekjoon/BinarySearch/src/랜선자르기_1654.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 랜선자르기_1654 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
