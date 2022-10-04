0c24c2571b1545750fbc9e83b6882885e69653c0
username <ieejo716@naver.com>
2022-10-04 23:30:07 +0900

    [CHORE] 복붙용

---
+++ Java/Baekjoon/DefaultInput.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class DefaultInput {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        inputs();
+    }
+
+    private static void inputs() throws IOException{
+
+    }
+}
