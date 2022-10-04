286ec8f1f6ce488c9648db98390cb7b0b4b09eeb
username <ieejo716@naver.com>
2022-10-04 23:58:15 +0900

    형식

--- Java/Baekjoon/DefaultInput.java
+++ Java/Baekjoon/DefaultInput.java
@@ -10,9 +10,15 @@ public class DefaultInput {
 
     public static void main(String[] args) throws IOException {
         inputs();
+        solution();
+        System.out.println(sb.toString());
     }
 
-    private static void inputs() throws IOException{
+    private static void solution() {
+
+    }
+
+    private static void inputs() throws IOException {
 
     }
 }
