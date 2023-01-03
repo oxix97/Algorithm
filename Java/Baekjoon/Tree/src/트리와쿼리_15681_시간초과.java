a656f8a601723ed763685a0894b0db1be7dc2b7a
username <ieejo716@naver.com>
2023-01-03 23:55:12 +0900

    [G5] : 트리와 쿼리 _ 시간초과

--- Java/Baekjoon/Tree/src/트리와쿼리_15681_시간초과.java
+++ Java/Baekjoon/Tree/src/트리와쿼리_15681_시간초과.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class 트리와쿼리_15681 {
+public class 트리와쿼리_15681_시간초과 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
