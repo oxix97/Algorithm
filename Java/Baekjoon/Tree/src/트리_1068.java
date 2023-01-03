f57c05b15c3134949c754994f30702b94c302a3a
username <ieejo716@naver.com>
2023-01-03 21:19:06 +0900

    [G5] : 트리 _ 실패

---
+++ Java/Baekjoon/Tree/src/트리_1068.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class 트리_1068 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int N, erase;
+    static ArrayList<Integer>[] tree;
+    static int[] leaf;
+    static int ans = 0;
+    static int root;
+
+    public static void main(String[] args) throws IOException {
+        inputs();
+        solution();
+    }
+
+    private static void solution() {
+        if (root != erase)
+            dfs(root);
+
+        System.out.println(leaf[root]);
+    }
+
+    private static void dfs(int n) {
+        if (n == erase) return;
+
+        if (tree[n].isEmpty()) {
+            leaf[n] = 1;
+        }
+        for (int x : tree[n]) {
+            dfs(x);
+            leaf[n] += leaf[x];
+        }
+    }
+
+    private static void inputs() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        tree = new ArrayList[N];
+        leaf = new int[N];
+
+        for (int i = 0; i < N; i++)
+            tree[i] = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            if (n == -1) {
+                root = i;
+                continue;
+            }
+            tree[n].add(i);
+        }
+
+        erase = Integer.parseInt(br.readLine());
+    }
+}
