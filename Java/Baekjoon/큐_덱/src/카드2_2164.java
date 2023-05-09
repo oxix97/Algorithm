cc9b6ea62bc8818db91109a287906992c61bfd89
Chan <ieejo716@naver.com>
2023-05-09 13:21:21 +0900

    [S4] 카드2 - Queue

--- Java/Baekjoon/큐_덱/src/카드2_2164.java
+++ Java/Baekjoon/큐_덱/src/카드2_2164.java
@@ -1,28 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class 카드2_2164 {
+    //1. 각 입력값들의 파라미터 정리
+//2. 시간복잡도, 공간복잡도 계산
+//3. 올바른 풀이방법 설정 이후 부가적으로 필요한 파라미터가 있는지 체크
+//4. 단계별로 sudo 코드 작성
+//5. 구현
+//6. 오류 발생시 디버그를 활용하거나 자료형 확인
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
     static int N;
+    static Queue<Integer> q;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        Queue<Integer> list = new LinkedList<>();
+        inputs();
+        solution();
+        output();
+        br.close();
+    }
 
+    private static void solution() {
+        initialQueue();//1. 큐에 N만큼 넣는다.
+
+        if (q.size() == 1) { //2. q.poll(), q.poll() q.add() 반복
+            sb.append(q.poll());
+        } else if (q.size() == 2) {
+            q.poll();
+            sb.append(q.poll());
+        } else {
+            shuffle();
+            sb.append(q.poll());
+        }
+    }
+
+    private static void shuffle() {
+        while (q.size() != 1) {
+            q.poll();
+            q.add(q.poll());
+        }
+    }
+
+    private static void initialQueue() {
         for (int i = 1; i <= N; i++) {
-            list.offer(i);
+            q.add(i);
         }
+    }
 
-        while (list.size() != 1) {
-            list.remove();
-            int tmp = list.poll();
-            list.add(tmp);
-        }
-        System.out.println(list.peek());
+    private static void output() {
+        System.out.println(sb.toString());
+    }
+
+    private static void inputs() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        q = new LinkedList<>();
     }
 }
