dbeab085cf1b947a3bf234536d1125350d8b01c2
Chan <ieejo716@naver.com>
2023-05-10 12:17:20 +0900

    [B2] 수 정렬하기 - Bubble Sort

--- Java/Baekjoon/정렬/src/수정렬하기.java
+++ Java/Baekjoon/정렬/src/수정렬하기.java
@@ -1,13 +1,64 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class 수정렬하기 {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int cont = scan.nextInt();
-        int[] arr = new int[cont];
-        for(int i=0;i<cont;i++)
-            arr[i] = scan.nextInt();
-        Arrays.sort(arr);
-        for(int i=0;i<cont;i++)
-            System.out.println(arr[i]);
+    //1. 각 입력값들의 파라미터 정리
+    //2. 시간복잡도, 공간복잡도 계산
+    //3. 올바른 풀이방법 설정 이후 부가적으로 필요한 파라미터가 있는지 체크
+    //4. 단계별로 sudo 코드 작성
+    //5. 구현
+    //6. 오류 발생시 디버그를 활용하거나 자료형 확인
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        inputs();
+        solution();
+        output();
+        br.close();
+    }
+
+    private static void solution() {
+        for (int i = 0; i < N; i++) {
+            boolean swap = false;
+            for (int j = 0; j < N - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap = true;
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+            if (!swap) break;
+        }
+    }
+
+    private static void output() {
+        for (int n : arr) {
+            appendVertical(n);
+        }
+        System.out.println(sb);
+    }
+
+    private static void inputs() throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+    }
+
+    private static void appendVertical(Object obj) {
+        sb.append(obj).append('\n');
+    }
+
+    private static void appendHorizontal(Object obj) {
+        sb.append(obj).append(' ');
     }
 }
