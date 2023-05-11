f177f712fb53c2b156f855473344aed9c684817f
Chan <ieejo716@naver.com>
2023-05-12 02:16:42 +0900

    [S2] DFS와 BFS - 1260 : DFS-BFS

---
+++ Java/Baekjoon/src/search/bfs/DFS와BFS_1260.java
@@ -0,0 +1,101 @@
+package src.search.bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class DFS와BFS_1260 {
+    //1. 각 입력값들의 파라미터 정리
+    //2. 시간복잡도, 공간복잡도 계산
+    //3. 올바른 풀이방법 설정 이후 부가적으로 필요한 파라미터가 있는지 체크
+    //4. 단계별로 sudo 코드 작성
+    //5. 구현
+    //6. 오류 발생시 디버그를 활용하거나 자료형 확인
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int N, M, V;
+    static boolean[] visited;
+    static ArrayList<Integer>[] list;
+
+    public static void main(String[] args) throws IOException {
+        inputs();
+        solution();
+        output();
+        br.close();
+    }
+
+    private static void inputs() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        list = new ArrayList[N + 1];
+
+        for (int i = 0; i <= N; i++)
+            list[i] = new ArrayList<>();
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list[a].add(b);
+            list[b].add(a);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(list[i]);
+        }
+    }
+
+    private static void solution() {
+        dfs(V);
+        Arrays.fill(visited, false);
+        sb.append('\n');
+        bfs(V);
+    }
+
+    private static void dfs(int n) {
+        visited[n] = true;
+        appendHorizontal(n);
+
+        for (int x : list[n]) {
+            if (!visited[x]) {
+                dfs(x);
+            }
+        }
+    }
+
+    private static void bfs(int n) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(n);
+        visited[n] = true;
+
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            appendHorizontal(x);
+
+            for (int i : list[x]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+    }
+
+    private static void output() {
+        System.out.println(sb.toString());
+    }
+
+    private static void appendVertical(Object obj) {
+        sb.append(obj).append('\n');
+    }
+
+    private static void appendHorizontal(Object obj) {
+        sb.append(obj).append(' ');
+    }
+}
