2d6eb71e05133e6e30f6e443ad7439605def059e
Chan <ieejo716@naver.com>
2023-05-13 17:59:41 +0900

    [S4] 수찾기 - 1920 : binary search

---
+++ Java/Baekjoon/src/search/binary_search/수찾기_1920.java
@@ -0,0 +1,77 @@
+package src.search.binary_search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 수찾기_1920 {
+    //1. 각 입력값들의 파라미터 정리
+    //2. 시간복잡도, 공간복잡도 계산
+    //3. 올바른 풀이방법 설정 이후 부가적으로 필요한 파라미터가 있는지 체크
+    //4. 단계별로 sudo 코드 작성
+    //5. 구현
+    //6. 오류 발생시 디버그를 활용하거나 자료형 확인
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int N, M;
+    static int[] inputs;
+    static int[] targets;
+
+    public static void main(String[] args) throws IOException {
+        inputs();
+        solution();
+        output();
+        br.close();
+    }
+
+    private static void inputs() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        inputs = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            inputs[i] = Integer.parseInt(st.nextToken());
+        }
+
+        M = Integer.parseInt(br.readLine());
+        targets = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            targets[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    private static void solution() {
+        Arrays.sort(inputs);
+        for (int n : targets) {
+            appendVertical(binarySearch(n));
+        }
+    }
+
+    private static int binarySearch(int n) {
+        int left = 0;
+        int right = N - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (n == inputs[mid]) return 1;
+            else if (n > inputs[mid]) left = mid + 1;
+            else right = mid - 1;
+        }
+        return 0;
+    }
+
+    private static void output() {
+        System.out.println(sb.toString());
+    }
+
+    private static void appendVertical(Object obj) {
+        sb.append(obj).append('\n');
+    }
+
+    private static void appendHorizontal(Object obj) {
+        sb.append(obj).append(' ');
+    }
+}
