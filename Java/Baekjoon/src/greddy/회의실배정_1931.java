1f23980676cc65afc2f184e8caab6ef8a56814e7
Chan <ieejo716@naver.com>
2023-05-15 00:09:06 +0900

    [S1] 회의실 배정 - 1931 : Greedy(Array Comparator 사용)

---
+++ Java/Baekjoon/src/greddy/회의실배정_1931.java
@@ -0,0 +1,68 @@
+package src.greddy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class 회의실배정_1931 {
+    //1. 각 입력값들의 파라미터 정리
+    //2. 시간복잡도, 공간복잡도 계산
+    //3. 올바른 풀이방법 설정 이후 부가적으로 필요한 파라미터가 있는지 체크
+    //4. 단계별로 sudo 코드 작성
+    //5. 구현
+    //6. 오류 발생시 디버그를 활용하거나 자료형 확인
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int N;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        inputs();
+        solution();
+        output();
+        br.close();
+    }
+
+    private static void inputs() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    private static void solution() {
+        sortArr(); //종료시간 기준으로 정렬, 동일한 경우 시작 시간 빠른순으로 정렬(로직 상 안될수 있어서)
+        int cnt = 0, end = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i][0] >= end) {
+                end = arr[i][1];
+                ++cnt;
+            }
+        }
+        sb.append(cnt);
+    }
+
+    private static void sortArr() {
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+    }
+
+    private static void output() {
+        System.out.print(sb.toString());
+    }
+}
