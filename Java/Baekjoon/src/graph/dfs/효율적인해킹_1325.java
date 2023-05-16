bfb35d3625dbbce940f395c92febd41110ee6dfb
Chan <ieejo716@naver.com>
2023-05-16 15:41:08 +0900

    [S1] 효율적인 해킹 1325 : DFS

--- Java/Baekjoon/src/graph/dfs/효율적인해킹_1325.java
+++ Java/Baekjoon/src/graph/dfs/효율적인해킹_1325.java
@@ -6,12 +6,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class 효율적인해킹_1325 {
-    //1. 각 입력값들의 파라미터 정리
-    //2. 시간복잡도, 공간복잡도 계산
-    //3. 올바른 풀이방법 설정 이후 부가적으로 필요한 파라미터가 있는지 체크
-    //4. 단계별로 sudo 코드 작성
-    //5. 구현
-    //6. 오류 발생시 디버그를 활용하거나 자료형 확인
+
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
@@ -51,7 +46,7 @@ public class 효율적인해킹_1325 {
     private static void solution() {
         int max = 0;
         for (int i = 1; i <= N; i++) {
-            bfs(i);
+            dfs(i);
             Arrays.fill(visited, false);
         }
 
@@ -62,27 +57,20 @@ public class 효율적인해킹_1325 {
         output(max);
     }
 
-    private static void bfs(int n) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(n);
+    private static void dfs(int n) {
         visited[n] = true;
 
-        while (!q.isEmpty()) {
-            int x = q.poll();
-            for (int nx : list[x]) {
-                if (!visited[nx]) {
-                    visited[nx] = true;
-                    q.add(nx);
-                    answer[nx]++;
-                }
+        for (int nx : list[n]) {
+            if (!visited[nx]) {
+                dfs(nx);
+                answer[nx]++;
             }
         }
     }
 
     private static void output(int max) {
         for (int i = 1; i <= N; i++) {
-            if (max == answer[i])
-                appendHorizontal(i);
+            if (max == answer[i]) appendHorizontal(i);
         }
         System.out.print(sb);
     }
