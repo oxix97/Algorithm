5fabaf34379beda6dbbf88155e989109441cf9a4
Chan <ieejo716@naver.com>
2023-05-20 22:19:52 +0900

    [S1] 쉬운계단수 10844 : DP

---
+++ Java/Baekjoon/src/dynamic/쉬운게단수_10844.java
@@ -0,0 +1,23 @@
+package src.dynamic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 쉬운게단수_10844 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        //1. 각 입력값들의 파라미터 정리
+        //2. 시간복잡도, 공간복잡도 계산
+        //3. 올바른 풀이방법 설정 이후 부가적으로 필요한 파라미터가 있는지 체크
+        //4. 단계별로 sudo 코드 작성
+        //5. 구현
+        //6. 오류 발생시 디버그를 활용하거나 자료형 확인
+        st = new StringTokenizer(br.readLine());
+        br.close();
+    }
+}
