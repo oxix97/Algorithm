95dc186812649f59d565a0ef8ef528f4f0cc62b9
Chan <ieejo716@naver.com>
2023-05-20 22:20:07 +0900

    [D2] 최대 상금 1244

---
+++ Java/Baekjoon/src/sw/최대상금_1244.java
@@ -0,0 +1,19 @@
+package src.sw;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class 최대상금_1244 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int T = sc.nextInt();
+
+        for (int i = 1; i <= T; i++) {
+            int num = sc.nextInt();
+            int cnt = sc.nextInt();
+
+
+        }
+    }
+}
